b01c1ad1de011a4b91a6efd3b2c26095f46ca49d
George Dvorak <unicodegeorge@gmail.com>
2025-10-02 14:03:30 +0200

    unlockCost

--- Models/Ability.cs
+++ Models/Ability.cs
@@ -5,14 +5,15 @@ public class Ability
     public string Name { get; set; }
     public string Description { get; set; }
     public float Cooldown { get; set; }
-    public int UnlockCost { get; set; } = 0;
+    public int UnlockCost { get; set; }
 
 
-    public Ability(string name, string description, float cooldown)
+    public Ability(string name, string description, float cooldown, int unlockCost)
     {
         Name = name;
         Description = description;
         Cooldown = cooldown;
+        UnlockCost = unlockCost;
     }
 
     public virtual void Execute(CCSPlayerController player)
